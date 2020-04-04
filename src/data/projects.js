faf543a5db3b8071cba1f328083f0384be3e2929
Kats <katsu.0323.jpn@icloud.com>
2020-04-04 19:20:31 +0900

    making music app

---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS = [
+    {
+        id: 1,
+        title: 'Example Reacr Application',
+        description: 'A React App that I built',
+        link: 'https://github.com/15Dkatz/ReactJS_VideoGuides',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'My api',
+        description: 'A React App that I built',
+        link: 'https://github.com/15Dkatz/ReactJS_VideoGuides',
+        image: project2
+    },
+    {
+        id: 3,
+        title: 'Final project',
+        description: 'A React App that I built',
+        link: 'https://github.com/15Dkatz/ReactJS_VideoGuides',
+        image: project3
+    }
+]
+
+export default PROJECTS
